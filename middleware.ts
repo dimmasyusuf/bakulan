03799eb3bae547c73626eef6fa3a7aba192529c3
dimmasyusuf <dimasyusufqurohman@gmail.com>
2024-02-28 22:37:59 +0700

    feat: install and configure next auth, prisma, and mongodb

---
+++ middleware.ts
@@ -0,0 +1,39 @@
+import NextAuth from "next-auth";
+
+import authConfig from "@/auth.config";
+
+const { auth: middleware } = NextAuth(authConfig);
+
+const publicRoutes = ["/"];
+
+const authRoutes = ["/dashboard"];
+
+export default middleware((req) => {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith("/api/auth");
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if (isApiAuthRoute) {
+    return;
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL("/", nextUrl));
+    }
+    return;
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL("/", nextUrl));
+  }
+
+  return;
+});
+
+export const config = {
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
