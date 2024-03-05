8e50a74d06528f4a7da6c45d453d368a932f3535
dimmasyusuf <dimasyusufqurohman@gmail.com>
2024-03-05 14:01:55 +0700

    style: create auth dialog

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,13 @@
-import React from "react";
 import Link from "next/link";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Image from "next/image";
-import { Button } from "./ui/button";
+import { Button } from "@/components/ui/button";
+import AuthDialog from "@/components/AuthDialog";
+import { auth } from "@/auth";
 
-export default function Navbar() {
+export default async function Navbar() {
+  const session = await auth();
+
   return (
     <nav className="mx-auto flex max-w-screen-2xl items-center justify-between">
       <Link href="/" className="cursor-pointer">
@@ -30,10 +33,14 @@ export default function Navbar() {
       </ul>
 
       <div className="flex items-center gap-8">
-        <Avatar>
-          <AvatarImage src="https://github.com/shadcn.png" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
+        {session ? (
+          <Avatar>
+            <AvatarImage src={session?.user?.image!} />
+            <AvatarFallback>{session?.user?.name}</AvatarFallback>
+          </Avatar>
+        ) : (
+          <AuthDialog />
+        )}
       </div>
     </nav>
   );
