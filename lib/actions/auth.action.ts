3c4e718bc844f2107446793387d45e77e7bdc8c3
dimmasyusuf <dimasyusufqurohman@gmail.com>
2024-03-09 15:09:20 +0700

    feat: create createUser auth action

---
+++ lib/actions/auth.action.ts
@@ -0,0 +1,41 @@
+"use server";
+
+import { z } from "zod";
+import { registerFormSchema } from "../validator";
+import bcrypt from "bcrypt";
+import prisma from "../db";
+
+export const createUser = async (
+  values: z.infer<typeof registerFormSchema>,
+) => {
+  const validatedFields = registerFormSchema.safeParse(values);
+
+  if (!validatedFields.success) {
+    return { error: "Invalid fields!" };
+  }
+
+  const { name, email, password } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (existingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: send verification email token
+
+  return { success: "User created!" };
+};
