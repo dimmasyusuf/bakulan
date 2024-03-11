db5586272c0d5e01ac877081e3f5ea4051c3757f
dimmasyusuf <dimasyusufqurohman@gmail.com>
2024-03-11 10:27:01 +0700

    feat: add credentials auth provider

--- auth.config.ts
+++ auth.config.ts
@@ -1,7 +1,29 @@
-import Google from "next-auth/providers/google";
-
 import type { NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import bcrypt from "bcrypt";
+
+import { loginFormSchema } from "./lib/validator";
+import { getUserByEmail } from "./lib/actions/user.action";
 
 export default {
-  providers: [Google],
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const validatedFields = loginFormSchema.safeParse(credentials);
+
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+
+          const user = await getUserByEmail(email);
+          if (!user || !user.password) return null;
+
+          const passwordsMatch = await bcrypt.compare(password, user.password);
+
+          if (passwordsMatch) return user;
+        }
+
+        return null;
+      },
+    }),
+  ],
 } satisfies NextAuthConfig;
