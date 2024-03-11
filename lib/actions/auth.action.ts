c7866b7fc60cc0cd1b6818042869b74afa872b35
dimmasyusuf <dimasyusufqurohman@gmail.com>
2024-03-11 10:37:26 +0700

    feat: create loginUser server action

--- lib/actions/auth.action.ts
+++ lib/actions/auth.action.ts
@@ -1,10 +1,12 @@
 "use server";
 
 import { z } from "zod";
-import { registerFormSchema } from "../validator";
+import { loginFormSchema, registerFormSchema } from "../validator";
 import bcrypt from "bcrypt";
 import prisma from "../db";
 import { getUserByEmail } from "./user.action";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
 
 export const createUser = async (
   values: z.infer<typeof registerFormSchema>,
@@ -35,4 +37,29 @@ export const createUser = async (
   // TODO: send verification email token
 
   return { success: "User created!" };
+};
+
+export const loginUser = async (values: z.infer<typeof loginFormSchema>) => {
+  const validatedFields = loginFormSchema.safeParse(values);
+
+  if (!validatedFields.success) {
+    return { error: "Invalid fields!" };
+  }
+
+  const { email, password } = validatedFields.data;
+
+  try {
+    await signIn("credentials", { email, password, redirectTo: "/" });
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case "CredentialsSignin":
+          return { error: "Invalid credentials!" };
+        default:
+          return { error: "Something went wrong!" };
+      }
+    }
+
+    throw error;
+  }
 };
