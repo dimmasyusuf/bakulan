d5b62726b6c0a7257b15afaa84f43c0e673b123e
dimmasyusuf <dimasyusufqurohman@gmail.com>
2024-03-16 17:33:59 +0700

    style: create email template for reset password

--- lib/actions/resend.action.ts
+++ lib/actions/resend.action.ts
@@ -3,18 +3,23 @@
 import { Resend } from "resend";
 import { z } from "zod";
 import { sendEmailFormSchema } from "../validator";
+import Email from "@/components/Email";
+import { getUserByEmail } from "./user.action";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (
   values: z.infer<typeof sendEmailFormSchema>,
 ) => {
+  const user = await getUserByEmail(values.email);
+  const name = user?.name || "there";
+
   try {
     await resend.emails.send({
       from: "Bakulan <onboarding@resend.dev>",
       to: values.email,
       subject: "Reset Password",
-      html: `<p>Click <a href="http://localhost:3000/reset-password">here</a> to reset your password</p>`,
+      react: Email({ name }),
     });
 
     return {
